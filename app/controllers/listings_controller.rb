63dae1e51bef63e124e7428220d09e27ab04ee4b
Jeff See <jeffsee@Jeffs-MacBook-Air.local>
2015-07-09 22:18:02 +0200

    Caching

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,4 @@
 class ListingsController < ApplicationController
-  caches_action :show
-
   def show
     @listing = Listing.find(params[:id])
   end
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,6 +1,4 @@
 class SectionsController < ApplicationController
-  caches_action :show
-  
   def show
     @section = Section.find(Shop.instance, params[:id])
   end
